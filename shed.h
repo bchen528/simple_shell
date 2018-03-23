70c2c763747d7a829b02d2c50c2a103554a7daef
adentintime <arixbizz@gmail.com>
2018-03-23 22:32:38 +0000

    add _strlen.c and shed.h, removing file

---
+++ shed.h
@@ -0,0 +1,18 @@
+#ifndef SHED_H
+#define SHED_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define win(x) write(STDOUT_FILENO, x, _strlen(x))
+#define din() write(STDOUT_FILENO, "###\n", 4)
+
+int _strlen(char *s);
+
+#endif /* SHED_H */
