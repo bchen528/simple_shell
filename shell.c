dd40e13ccf3b09a80ebcd2fe77b659d9967cc7fa
adentintime <arixbizz@gmail.com>
2018-03-26 17:33:07 +0000

    initial commit of shell.c and string_token.c to be renamed

---
+++ shell.c
@@ -0,0 +1,106 @@
+#include "head.h"
+
+int main(int argc, char **argv, char **env)
+{
+/* O make PATH robust and use strcmp to find */
+	char **path = (string_token(env[9], ":=") + 1);
+/* o FREE path! memory leak! */
+	ssize_t run_shell = 0;
+	size_t count = 0;
+	char *line = NULL, **array = NULL, *ptr = NULL;
+	int i = 0, exit_code = 0, call_count = 0;
+
+	while (42)
+	{
+		run_shell = 0;
+		count = 0;
+		line = NULL;
+		array = NULL;
+		ptr = NULL;
+		i = 0;
+		exit_code = 0;
+		call_count = 0;
+
+		call_count++;
+
+		write(STDIN_FILENO, "\033[1;35m$\033[0m ", 13);
+		run_shell = getline(&line, &count, stdin);
+		if (run_shell == -1)
+		{
+			write(2, "\n", 1);
+			return(0);
+/*
+			perror("Arik, getline failed");
+			line = NULL;
+*/
+		}
+		array = string_token(line, "\n ");
+		if (array == NULL)
+		free(line);
+		if (array != NULL && !(strcmp(array[0], "exit")))
+		{
+/*			write(2, "sh: 1: exit: Illegal number: ", 29);
+			write(2, array[0], _strlen(array[0]));
+			write(2, "\n", 1);
+*/
+/* OOO
+make custom atoi that returns -1 when being asked to convert something
+not number and print out "sh: 1: exit: Illegal number: %s", array[1]
+if not number
+			exit_code = array[1];
+
+			write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+			write(STDERR_FILENO, ": ", 2);
+			write(STDERR_FILENO, call_count, _strlen(?:));
+			write(STDERR_FILENO, ": exit", 6);
+					fix call_count number to print istead of buffer of 100
+					exit_code = array[1]; if not string
+*/			break;
+		}
+		if(array != NULL && !(strcmp(array[0], "env")))
+		{
+			print_env(env);
+		}
+/*		if (array == NULL)
+			array = NULL;
+*/
+		if (array != NULL && access(array[0], F_OK) == -1)
+			array[0] = path_helper(path, array[0]);
+
+		if (forking_helper(array))
+		{
+/* if fail do thing */
+		}
+/* OO
+create error message that includes 'call_count'
+name of program argv[0]
+maybe create a pointer 'ptr' for function (i.e. 'ls') and free it later?
+*/
+/*
+		if (execve(array[0], array, NULL) == -1)
+			perror("Arik, execve failed");
+
+where should we have errors?
+in this main func or in the helper funcs?
+*/
+		if (array != NULL)
+		{
+			free(array[0]);
+			free(array);
+		}
+	}
+/*	free(ptr);
+	free(path); */
+	free(array[0]);
+	free(array);
+/*
+free 'array' if NULL or not?
+	if (array != NULL)
+		free(array);
+
+	if (line != NULL)
+		free(line);
+*/
+
+	return (exit_code);
+}
---
+++ string_token.c
@@ -0,0 +1,56 @@
+#include "head.h"
+
+char **string_token(char *in, char *delim)
+{
+	int i;
+	char *in_1, *in_2, *ptr, **ar;
+
+	if (!(in) || !(delim))
+		return (NULL);
+/* ^ do we need? ^ */
+	in_1 = NULL;
+	in_2 = NULL;
+	ptr = NULL;
+	ar = NULL;
+/*duplicate string*/
+	in_1 = strdup(in);
+	in_2 = strdup(in);
+	if (in_1 == NULL)
+		return (NULL);
+/*find number of tokens*/
+	ptr = strtok(in_1, delim);
+/*check if strtok failed*/
+	if (ptr == NULL)
+		return (NULL);
+/*loop, count tokens*/
+	for (i = 0; ptr != NULL; i++)
+	{
+		ptr = strtok(NULL, delim);
+	}
+	ar = malloc((sizeof(char *) * i) + 1);
+	if (ar == NULL)
+		return (NULL);
+
+	ptr = strtok(in_2, delim);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; ptr != NULL; i++)
+	{
+		ar[i] = ptr;
+		ptr = strtok(NULL, delim);
+	}
+
+	ar[i] = NULL;
+
+/*
+free 'ptr'? free 'ar' back in main??
+changed to '\0'
+ar[i] = NULL; //what i had before
+I put it back to NULL
+*/
+
+	free(in_1);
+
+	return (ar);
+}
